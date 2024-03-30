3492ff66c3a84a80dad548957866105dc5304bb9
TharinduWijayarathna <tharinduwijayarathne87@gmail.com>
2024-03-30 13:40:49 +0530

    added models and some structural changes

--- base/views.py
+++ base/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-rooms = [
-    {'id': 1, 'name': 'Let\'s learn Django'},
-    {'id': 2, 'name': 'Let\'s learn React'},
-    {'id': 3, 'name': 'Let\'s learn Python'},
-    {'id': 4, 'name': 'Let\'s learn JavaScript'},
-    {'id': 5, 'name': 'Let\'s learn HTML'},
-    {'id': 6, 'name': 'Let\'s learn CSS'},
-]
+from .models import Room
 
 def home(request):
+    rooms = Room.objects.all() # Get all rooms from the database
     return render(request, 'base/home.html', {'rooms': rooms})
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
-
     return render(request, 'base/room.html', context)
